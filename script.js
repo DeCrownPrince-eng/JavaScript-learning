84b0162aa78646aaa29b8dc4ee4b94060693eb50
Ramoni Olu Okeowo <crownprince09@gmail.com>
2025-10-04 16:26:08 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,95 @@
+// 1.Numbers
+let age = 9999999999999;
+
+
+const price = 0;
+const cost = false;
+console.log("Check for equality");
+
+const quantity = 20;
+
+
+console.log("type of age", typeof age);
+console.log("type of price", typeof price);
+
+// 2. Strings
+let name = "Seyi";
+let greeting = "Good afternoon, Everyone";
+
+//3. Booleans
+let isClassOnline = true;
+let hasArrived = false;
+
+//4. Null
+let car = null;
+
+// 5.Undefined
+let address;
+
+//6. Objects
+let mobilePhone = {
+  name: "Samsung",
+  releaseYear: 2024,
+  isNew: false,
+};
+
+// 7. Symbols
+const mySymbol = Symbol(1);
+
+console.log("mySymbol", mySymbol);
+const anotherSymbol = Symbol(1);
+
+// 8. BigInts
+let bigNum = 123456789012345678901234567890n;
+console.log("bigNum", bigNum);
+let y = BigInt("123456789012345678901234567890");
+let x = BigInt (123456789012345678901234567890n)
+
+/* Exercise 1.
+Create a variable called name and assign a name to it.
+Create a variable called hobby and assign a hobby to it.
+Print these values on your console.
+*/
+const firstname = "Akeem";
+const hobby = "Learning";
+
+console.log(firstname, hobby);
+
+/* Exercise 2.
+Create two numbers variables: a, b and initialize both of them to 40
+a. Create a variable called sum and assign it to the sum of these two numbers.
+b. Create a variable called difference and assign it to the difference between these two numbers.
+c. Create a variable called product and assign it to the product of these two numbers
+d. Create a variable called division and assign it to quotient of the two values
+
+print each operation to the console using their respective variables
+
+*/
+
+const a = 40;
+const b = 40;
+console.log(a, b);
+
+const sum = a + b;
+const myVariable = 40(sum);
+console.log("sim", sum, 7, false);
+const my = a - b;
+console.log(difference);
+const product = a * b;
+console.log(product);
+const division = a / b;
+console.log(division);
+
+// console.log(sum, difference, product, division)
+
+/* Exercise 3.
+Create a variable hobby but don't give it a value
+Create another variable variable called item and set it to null.
+
+*/
+
+/* Exercise 3.
+Create a variable hobby but don't give it a value
+Create another variable variable called item and set it to null.
+
+*/
